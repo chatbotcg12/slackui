2dfef1340a047ecc8962b79cece5bfa1bef5d7f2
amritacapgemin <amrita.gupta@capgemini.com>
2019-05-23 14:34:50 +0530

    test ui code

---
+++ app.js
@@ -0,0 +1,40 @@
+
+var express = require('express');
+var app = express();
+
+
+/**set port using env variable */
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function () {
+    console.log("Listening on --- Port 3000");
+});
+
+Slack = require('node-slackr');
+slack = new Slack('https://hooks.slack.com/services/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');
+app.get('/getimage', (req, res) => {
+    messages = {
+        text: "Display Image",
+        channel: "#notifications",
+         attachments: [
+          {
+            color: '#5A352D',
+            title: 'How can I help you?',
+            callback_id: 'order:start',
+            actions: [
+              {
+                name: 'start',
+                text: 'Start a coffee order',
+                type: 'button',
+                value: 'order:start',
+              },
+            ],
+          },
+        ],
+
+    };
+    console.log("Image sent to slack");
+    slack.notify(messages, function (err, result) {
+        console.log(err, result);
+    });
+
+});
